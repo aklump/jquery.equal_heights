badd2a1f5a5aa5cba6adbc72b1b9a9e5b26e1fb8
aklump <sourcecode@intheloftstudios.com>
2014-10-08 18:19:08 -0700

    Removed once parameter. Added beforeApply callback. Fixed the timer bug for responsive. Added respond method as prototype. Bug fix on target application. More tests. eq instances are now available at $.fn.equalHeights.instances. Changed default prefix to eqh

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,42 @@
+QUnit.test("Assert beforeApply() callback functions correctly.", function(assert) {
+  var $group = $('#group9');
+  var shimHeight = $group.find('.shim').outerHeight();
+  $group.equalHeights({
+    "responsive": true,
+    "beforeApply": function (eh, width) {
+      return width > 600;
+    }
+  });
+
+  var unapplied = $(window).width() < 600;
+  if (unapplied) {
+    assert.strictEqual($group.find('.eqh-target').length, 0, 'No targets because window is less than 600px.');
+    assert.strictEqual($('#item20').height(), shimHeight, 'child 1 has correct, natural height');
+    assert.notStrictEqual($('#item21').height(), shimHeight, 'child 2 has correct, natural height');
+    assert.notStrictEqual($('#item22').height(), shimHeight, 'child 3 has correct, natural height');
+  }
+  else {
+    assert.strictEqual($group.find('.eqh-target').length, 3, '3 targets because window is >= 600px.');
+    assert.strictEqual($('#item20').height(), shimHeight, 'child 1 has correct, equal height');
+    assert.strictEqual($('#item21').height(), shimHeight, 'child 2 has correct, equal height');
+    assert.strictEqual($('#item22').height(), shimHeight, 'child 3 has correct, equal height');
+  }
+  
+});
+
+QUnit.test("Assert complex example using filter and target.", function(assert) {
+  var $group = $('#group8');
+  $group.equalHeights({
+    "filter": '.column',
+    "target": '.column-inner',
+    "responsive": true
+  });
+  assert.ok($group.find('.first').hasClass('eqh-target'), 'First filtered element has target class.');
+  assert.ok($group.find('.first .column-inner').hasClass('eqh-target'), 'First target element has target class.');
+  assert.ok($group.find('.second').hasClass('eqh-target'), 'Second filtered element has target class.');
+  assert.ok($group.find('.second .column-inner').hasClass('eqh-target'), 'Second target element has target class.');
+});
+
 QUnit.test("Assert responsive works as expected.", function(assert) {
   var $group = $('#group7');
 
@@ -23,7 +62,7 @@ QUnit.test("Assert disable removes heights from targets.", function(assert) {
   assert.strictEqual($('#item16').height(), shimHeight, 'child has correct height');
 
   $group.equalHeights({'disable': true});
-  assert.strictEqual($group.hasClass('equal-heights-processed'), false, 'Processed class has been removed from element');
+  assert.strictEqual($group.hasClass('eqh-processed'), false, 'Processed class has been removed from element');
   assert.notStrictEqual($('#item16').height(), shimHeight, 'child has natural height');
 });
 
@@ -35,8 +74,8 @@ QUnit.test("Assert height is also applied to target", function(assert) {
     'target': '.inner'
   });
 
-  assert.ok($group.hasClass('equal-heights-processed'), 'Processed class is added to element');
-  assert.strictEqual($group.find('.equal-heights-target').length, 6, 'No children receive target class');
+  assert.ok($group.hasClass('eqh-processed'), 'Processed class is added to element');
+  assert.strictEqual($group.find('.eqh-target').length, 6, 'No children receive target class');
 
   assert.strictEqual($('#item12').height(), shimHeight, 'child has correct height');
   assert.strictEqual($('#item12 .inner').height(), shimHeight, 'target has correct height');
@@ -48,9 +87,9 @@ QUnit.test("Assert height is not applied to a 'not' item", function(assert) {
   $group.equalHeights({
     'not': '#item9'
   });
-  assert.ok($group.hasClass('equal-heights-processed'), 'Processed class is added to element');
-  assert.strictEqual($group.find('.equal-heights-target').length, 2, 'No children receive target class');
-  assert.strictEqual($('#item9.equal-heights-target').length, 0, 'Not child does not receive target class');
+  assert.ok($group.hasClass('eqh-processed'), 'Processed class is added to element');
+  assert.strictEqual($group.find('.eqh-target').length, 2, 'No children receive target class');
+  assert.strictEqual($('#item9.eqh-target').length, 0, 'Not child does not receive target class');
   assert.notStrictEqual($('#item9').height(), $('#item8').height(), 'not child does not have equal height to processed child.');
 });
 
@@ -59,8 +98,8 @@ QUnit.test("Assert no height is applied with a single child due to filter", func
   $group.equalHeights({
     'filter': '#item5'
   });
-  assert.ok($group.hasClass('equal-heights-processed'), 'Processed class is added to element');
-  assert.strictEqual($group.find('.equal-heights-target').length, 0, 'No children receive target class');
+  assert.strictEqual($group.hasClass('eqh-processed'), false, 'Processed class was not added to element');
+  assert.strictEqual($group.find('.eqh-target').length, 0, 'No children receive target class');
 });
 
 QUnit.test("Assert height is applied with multiple children.", function(assert) {
@@ -68,23 +107,24 @@ QUnit.test("Assert height is applied with multiple children.", function(assert) 
   var shimHeight = $group.find('.shim').outerHeight();
   
   $group.equalHeights();
-  assert.ok($group.hasClass('equal-heights-processed'), 'Processed class is added to element');
+  assert.ok($group.hasClass('eqh-processed'), 'Processed class is added to element');
   
   assert.strictEqual($('#item2').height(), shimHeight, 'Child 1 receives inline height');
-  assert.strictEqual($('#item2.equal-heights-target').length, 1, 'Child 1 receives target class');
+  assert.strictEqual($('#item2.eqh-target').length, 1, 'Child 1 receives target class');
   
   assert.strictEqual($('#item3').height(), shimHeight, 'Child 2 receives inline height');
-  assert.strictEqual($('#item3.equal-heights-target').length, 1, 'Child 2 receives target class');
+  assert.strictEqual($('#item3.eqh-target').length, 1, 'Child 2 receives target class');
   
   assert.strictEqual($('#item4').height(), shimHeight, 'Child 3 receives inline height');
-  assert.strictEqual($('#item4.equal-heights-target').length, 1, 'Child 3 receives target class');  
+  assert.strictEqual($('#item4.eqh-target').length, 1, 'Child 3 receives target class');  
 });
 
 QUnit.test("Assert no height is applied with a single child.", function(assert) {
-  $('#group1').equalHeights();
-  assert.strictEqual($('#group1.equal-heights-processed').length, 1, 'Processed class is added to element');
+  var $group = $('#group1');
+  $group.equalHeights();
+  assert.strictEqual($group.hasClass('eqh-processed'), false, 'Processed class was not added to element');
   assert.ok(typeof $('#item1').attr('height') === 'undefined', 'Child does not receive an inline height');
-  assert.strictEqual($('#item1.equal-heights-target').length, 0, 'Child does not receive target class');
+  assert.strictEqual($('#item1.eqh-target').length, 0, 'Child does not receive target class');
 });
 
 
@@ -94,8 +134,8 @@ QUnit.test("Assert no height is applied with a single child.", function(assert) 
 
 function resetDom() {
   $('div')
-  .removeClass('equal-heights-processed')
-  .removeClass('equal-heights-target')
+  .removeClass('eqh-processed')
+  .removeClass('eqh-target')
   .not('.inner')
   .height('');  
 }
@@ -108,15 +148,15 @@ function resetDom() {
 
 //   // Removes height and classes.
 //   $('div').not('.inner')
-//   .removeClass('equal-heights-target')
-//   .removeClass('equal-heights-processed')
+//   .removeClass('eqh-target')
+//   .removeClass('eqh-processed')
 //   .height('');  
 // });
 
 // QUnit.testDone(function (details) {
 //   $('div')
-//   .removeClass('equal-heights-processed')
-//   .removeClass('equal-heights-target')
+//   .removeClass('eqh-processed')
+//   .removeClass('eqh-target')
 //   .not('.inner')
 //   .height('');
 // })
